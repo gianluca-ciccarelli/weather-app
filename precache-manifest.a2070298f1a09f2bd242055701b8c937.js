118a3070d56b9d6f512a92ebb7a92419872a27e2
pipistrel0 <gianluca.ciccarelli25@gmail.com>
2020-01-19 16:44:11 -0300

    Updates

---
+++ precache-manifest.a2070298f1a09f2bd242055701b8c937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74ef2775ceac9a2bf3d39516abd737d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7ac8625494447340df2f",
+    "url": "/weather-app/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "b32579df4417369b9fe9",
+    "url": "/weather-app/static/css/main.bd671fde.chunk.css"
+  },
+  {
+    "revision": "7ac8625494447340df2f",
+    "url": "/weather-app/static/js/2.607bed6a.chunk.js"
+  },
+  {
+    "revision": "b32579df4417369b9fe9",
+    "url": "/weather-app/static/js/main.15bbd32a.chunk.js"
+  },
+  {
+    "revision": "be9e885704fbfeba1ceb",
+    "url": "/weather-app/static/js/runtime-main.fab476f6.js"
+  }
+]);
